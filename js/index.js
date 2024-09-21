be5c4453c1a5eb4862fec70a23646af5c8ec32ef
anaybhadury <bhadury9292@gmail.com>
2024-09-21 19:52:35 +0600

    added index all

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log('show js');
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('clicked by login');
+
+    const phoneNumber = document.getElementById('input-phone-number').value;
+    const pinNumber = document.getElementById('input-pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    if(phoneNumber === '123456789' && pinNumber === '123'){
+        console.log('bah keya sin hay');
+        window.location.href = './home.html'
+    }
+    else{
+        alert('Your phone or pin number is incorrect');
+    }
+    
+});
