4c2b6b76dcaa9fa35aedc24a6419cb9f2f2c1183
anaybhadury <bhadury9292@gmail.com>
2024-09-22 11:17:00 +0600

    added add money

--- js/home.js
+++ js/home.js
@@ -1,1 +1,21 @@
-console.log('home is here');
+document.getElementById('add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    
+    const addMoney = getInputValueById('input-add-money');
+    const pinNumber = getInputValueById('input-pin-number');
+
+    console.log(addMoney, pinNumber);
+
+    if(isNaN(addMoney)){
+        alert('Faild to access! Please try again..');
+        return;
+    }
+    if(pinNumber === 123){
+        const balance = getTextValueById('acc-balance');
+        const totalBalance = balance + addMoney;
+        document.getElementById('acc-balance').innerText = totalBalance;
+    }
+    else{
+        alert('Faild to access! Please try again..');
+    }
+});
